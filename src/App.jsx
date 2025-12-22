e81b86213442538e5b92baf660bf096786764137
tom mckenna <thomasmckenna12@gmail.com>
2025-12-22 17:39:23 -0600

    added header and footer init page

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import Header from './components/Header.js';
+import Header from './components/Header'
+import Layout from './components/Layout'
+import Footer from './components/Header'
 
 function App() {
   return (
     
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Header />} />
+        <Route path="/" element={<Layout />} >
+          
+        </Route>
       </Routes>
     </BrowserRouter>
   );
