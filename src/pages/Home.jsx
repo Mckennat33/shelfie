fe1c79fe245d69dd32af4d621275a254915a820e
tom mckenna <thomasmckenna12@gmail.com>
2025-12-23 20:47:52 -0600

    init fetch for bookdata

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,23 @@
 import react from 'react'
 
 
+
+
 function Home() {
+    async function fetchBooks() {
+        const url = 'https://amazon-books-data-api1.p.rapidapi.com/api/books?limit=150';
+        const options = {
+	        method: 'GET',
+	        headers: {
+		        'x-rapidapi-key': 'de51889a1fmshe095099b1a97993p13134fjsnc818ad7373cb',
+		        'x-rapidapi-host': 'amazon-books-data-api1.p.rapidapi.com'
+	            }
+            };
+            const bookData = await fetch(url, options)
+            const data = await bookData.json()
+    }    
+    fetchBooks()
+
     return (
         <>
             <h3>Home Page</h3>
