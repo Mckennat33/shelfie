bc9cfa47b42739b069792e1e1c5cdbf1e31ff47e
tom mckenna <thomasmckenna12@gmail.com>
2025-12-23 21:58:15 -0600

    worked on conditional rendering of profile nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import { NavLink  } from 'react-router-dom';
+import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
 import Profile from '../pages/Profile.jsx'
 
 function Header() {
-    return (
-        <div className='header'>
-            <NavLink to="/" className='shelfie-img' >Shelfie</NavLink>
-            <div className='right-nav'>
-                <p>Sign In</p>
-                <p>Create an Account</p>
-                <Profile />
-            </div>
-               
-        </div>
-    )
+  const [loading, setLoading] = useState(false)
+
+  return (
+    <div className='header'>
+      {loading ? (
+        <h1><Profile /></h1>
+      ) : (
+        <>
+          <NavLink to="/" className='shelfie-img'>Shelfie</NavLink>
+          <div className='right-nav'>
+            <p>Sign In</p>
+            <p>Create an Account</p>
+          </div>
+        </>
+      )}
+    </div>
+  )
 }
 
-export default Header;
+export default Header
