a065fb91069d39b62c4be1ee3800549cb3676697
tom mckenna <thomasmckenna12@gmail.com>
2025-12-24 17:07:57 -0600

    added all books to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 // import Books from '../bookdata/books'
 import Books from '../database/books.js'
+import Image from 'react'
 
-// console.log(Books)
+console.log(Books)
+
+// inside of Home what do we want to render
+// All books |  Books read  | TBR 
+
+const { author, description, genre, id, image_url, title, total_pages, year_published } = Books
+
 
 function Home() {
     return (
         <>
             <h3>Home Page</h3>
+            <NavLink>All Books</NavLink>
+            <div>
+                {Books.map((book) => {
+                    return (
+                        <div key={book.id} className='book-div'>
+                            <img src={book.image_url} />
+                            <p className='book-author'>Author: {book.author}</p>
+                            <p className='book-title'>Title: {book.title}</p>
+                            <p className='book-genre'>Genre: {book.genre}</p>
+                            <p className='book-description'>Descriptoin: {book.description}</p>
+                            <p className='book-pages'> Pages: {book.total_pages}</p>
+                        </div>
+                    )
+                })}
+            </div>
         </>
     )
 }
