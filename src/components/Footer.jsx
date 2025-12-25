1c174798a4f247ea4ae44d2f795786567788318e
tom mckenna <thomasmckenna12@gmail.com>
2025-12-24 18:22:51 -0600

    init reading graph text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,23 @@ function Footer() {
     return (
         <div className="footer">
             <h1>Footer</h1>
+            <div className="graph-main-card" >
+                <div className="header-bttns-container">
+                    <img src="" alt="" />
+                    <h3>Reading Progress</h3>
+                    
+                    <h3>Log Pages</h3>
+                </div>
+                <div className="page-counter-container">
+                    <h3>Total Pages</h3>
+                    <h3>Daily Average</h3>
+                </div>
+                <div className="graph-container">
+                    <h3>Graph goes here</h3>
+                </div>
+
+            </div>
+
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,17 +14,24 @@ function Home() {
     return (
         <>
             <h3>Home Page</h3>
-            <NavLink>All Books</NavLink>
+            <NavLink
+                to='/'
+                // need to figure this part out.
+            >
+                All Books
+            </NavLink>
             <div className='book-grid'>
-                {Books.slice(0, 18).map((book) => {
+                {Books.slice(0, 8).map((book) => {
                     return (
                         <div key={book.id} className='book-card'>
                             <img className='book-image' src={book.image_url} />
                             <p className='book-author'>Author: {book.author}</p>
                             <p className='book-title'>Title: {book.title}</p>
                             <p className='book-genre'>Genre: {book.genre}</p>
-                            <p className='book-description'>Descriptoin: {book.description}</p>
+                            <p className='book-description'>Description: {book.description}</p>
                             <p className='book-pages'> Pages: {book.total_pages}</p>
+                            <button>Read +</button>
+                            <button>Remove -</button>
                         </div>
                     )
                 })}
