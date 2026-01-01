cd6dd6e49594f280a0248ddfe07be22ae9b312d0
tom mckenna <thomasmckenna12@gmail.com>
2026-01-01 16:23:16 -0600

    connected to mysql database. Had to install mysql2

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
+
+const db = require('./config/db.js')
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8081
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello from the backend')
+
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
+
