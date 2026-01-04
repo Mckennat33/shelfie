572e9d5479fe28cadaa4b1108a0327e978493bb1
tom mckenna <thomasmckenna12@gmail.com>
2026-01-04 13:21:08 -0600

    added signup page

--- src/pages/Allbooks.jsx
+++ src/pages/Allbooks.jsx
@@ -72,3 +72,5 @@ const AllBooks = () => {
 }
 
 export default AllBooks; 
+
+
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+const SignUp = () => {
+    const 
+    return (
+        <>
+            <div>
+                <form method='POST'>
+                    <label htmlFor="username">Username</label>
+                    <input
+                        onChange={}
+                        type='username'
+                        value='username'
+                    />
+                    <label htmlFor="email">Email</label>
+                    <input
+                        onChange={}
+                        type='email'
+                        value='email'
+                    />
+                    <label htmlFor="password">Password</label>
+                    <input
+                        onChange={}
+                        type='password'
+                        value='password'
+                    />
+                </form>
+            </div>
+        </>
+    )
+}
